8d2f9940c589cb1e1ab8068bd651372eeb3d6d1a
Paul <isakura313@gmail.com>
2021-02-27 19:31:06 +0300

    asdsad

---
+++ app/src/main/java/com/example/spisok/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.spisok
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val taskListView = findViewById<ListView>(R.id.task_lv)
+
+
+        val taskList = ArrayList<String>()
+        taskList.add("Помыть кота")
+        taskList.add("Открыть банку огурцов")
+        taskList.add("Сделать домашку")
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, taskList)
+
+        taskListView.adapter = adapter
+    }
+}
